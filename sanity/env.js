fdd4bde2b32115a68f70d592799d78ff0eaa048b
kamta prasad <kamta_tiwari@yahoo.com>
2026-01-20 14:19:20 +0530

    fix sanity env

--- sanity/env.js
+++ sanity/env.js
@@ -1,5 +1,3 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-12-11'
-
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const apiVersion = '2025-12-11'
+export const dataset = 'production'
+export const projectId = 't0tcbatn'
